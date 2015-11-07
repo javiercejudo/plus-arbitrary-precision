aa8494b6653e4a6e7734ea30829a526ecd9f6f47
Javier Cejudo <javier@javiercejudo.com>
2015-11-08 10:27:15 +1100

    refactor: use binary-op-arbitrary-precision package

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@
 
 'use strict';
 
+var binaryOpExtender = require('binary-op-arbitrary-precision');
+
 module.exports = function factory(Decimal) {
-  return binaryFactory(Decimal, 'plus');
+  return binaryOpExtender(Decimal, 'plus');
 };
-
-function binaryFactory(Decimal, operation) {
-  Decimal.prototype[operation] = function(x) {
-    return new Decimal(Decimal.getAdapter()[operation](this.val(), x.val()).toString());
-  };
-
-  return Decimal;
-}
